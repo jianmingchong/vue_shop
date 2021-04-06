93e55a0564b6c03004c2c2e9fd26f6d99ce7ceb5
jmc <1170426727@qq.com>
2021-04-06 22:49:14 +0800

    add files

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-irregular-whitespace': 'off',
+    'no-multiple-empty-lines': 0,
+    'indent': 0
   }
 }
