21c2fff07b89e545bf6a19fc801f29e63ee20dc8
jmc <1170426727@qq.com>
2021-04-07 23:53:20 +0800

    login page

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,10 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-irregular-whitespace': 'off',
     'no-multiple-empty-lines': 0,
-    'indent': 0
+    'indent': 0,
+    'no-duplicate-imports': 0,
+    'space-before-function-paren': 0,
+    'semi': 0
+
   }
 }
