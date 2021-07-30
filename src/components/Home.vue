76f11198bac52b2778b16e88eb63c923f46f0b45
jmc <1170426727@qq.com>
2021-07-31 00:22:18 +0800

    ..

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -78,7 +78,7 @@
                 const { data: res } = await this.$http.get('menus');
                 if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
                 this.menulist = res.data;
-                console.log(res);
+                // console.log(res);
             },
             // 点击按钮 展开或折叠菜单
             toggleCollapse() {
