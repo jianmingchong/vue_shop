34762124c972b77b34284b468c49280574fea284
mingchong <1170426727@qq.com>
2021-08-27 15:35:48 +0800

    order

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,7 +37,9 @@ import {
     Steps,
     Checkbox,
     CheckboxGroup,
-    Upload
+    Upload,
+    Timeline,
+    TimelineItem
 } from 'element-ui'
 
 
@@ -77,6 +79,8 @@ Vue.use(Steps)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 // 消息提示组件需全局挂载
 Vue.prototype.$message = Message
